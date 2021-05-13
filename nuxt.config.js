a9c39c8401a29487e990ae21598a42353591cd56
zoking <1053189708@qq.com>
2021-05-13 17:20:04 +0800

    update: open ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'FEInterviewMap',
+    title: '程序之道',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
